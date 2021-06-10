0367c8081e3e8735154d1232d8928d1273505b78
star10919 <star10919@navercom>
2021-06-10 09:53:17 +0900

    today commit

--- backend/common/services.py
+++ backend/common/services.py
@@ -1,4 +1,7 @@
+import pandas as pd
+
 class CommonService(object):
+
 
     def print_this(self, this):
         print('*' * 100)
@@ -7,3 +10,15 @@ class CommonService(object):
         print(f'3. 대상의 상위 5개 행\n{this.head()} 이다.')
         print(f'4. 대상의 null 의 갯수\n {this.isnull().sum()}개')
         print('*' * 100)
+
+    def new_model_csv(self, payload) -> object:
+        this = self.crime_DTO
+        this.context = './data/'
+        this.fname = payload
+        return pd.read_csv(this.context + this.fname + '.csv')
+
+    def new_model_exel(self, payload) -> object:
+        this = self.crime_DTO
+        this.context = './data/'
+        this.fname = payload
+        return pd.read_excel(this.context + this.fname + '.xls')
--- backend/crime_cctv/services.py
+++ backend/crime_cctv/services.py
@@ -1,19 +1,8 @@
 from crime_cctv.models import CrimeDTO
 from common.services import CommonService
-import pandas as pd
-import xlwings as xw
+
 
 class CrimeService(CommonService):
+
     crime_DTO = CrimeDTO()
 
-    def new_model_csv(self, payload) -> object:
-        this = self.crime_DTO
-        this.context = './data/'
-        this.fname = payload
-        return pd.read_csv(this.context + this.fname + '.csv')
-
-    def new_model_exel(self, payload) -> object:
-        this = self.crime_DTO
-        this.context = './data/'
-        this.fname = payload
-        return pd.read_excel(this.context + this.fname + '.xls')
